50908e1c2a8aa7b888200dbbe1f9b471e41b6347
Gokul <gokuluniemail@gmail.com>
2022-11-03 20:36:12 +0530

    Initial Commit

---
+++ Shared/CartcheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  CartcheckApp.swift
+//  Shared
+//
+//  Created by gokul on 03/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CartcheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
